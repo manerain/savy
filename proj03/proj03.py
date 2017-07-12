91474672e076103db9fad33f13d900674b7f8faf
manerain <LailaS50453@mnpsk12.org>
2017-07-12 10:19:59 -0500

    Update proj03.py

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -10,12 +10,10 @@ print "Welcome to the Guessing Game!"
 enter = raw_input("Press ENTER")
 print ""
 number = random.randint(1,9)
-#guess = raw_input("Guess my number, 1 to 9(enter 0 to end game):")
+
 number = int(number)
-#True = guess != number
 loop = True
-#if guess == number:
-   # print "Congratulations, you guessed my number!"
+   
 
 
 
@@ -24,18 +22,18 @@ while loop == True:
     guess = raw_input("Guess my number, 1 to 9 (enter 0 to end game): ")
     guess = int(guess)
     if guess > number:
-        #Too high
+        
         print ""
         print "Your guess is too high."
         points = points - 1
 
     elif guess < number:
-        #End game
+       
         if guess == 0:
             loop = False
             print "\nYou exited the game."
             print "Your total points: ", points
-        #Too low
+     
         elif guess < number:
             print ""
             print "Your guess is too low."
