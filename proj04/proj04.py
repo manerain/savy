a89a56a2722cc628b0a6b29804ca19fe2a334301
manerain <LailaS50453@mnpsk12.org>
2017-07-12 15:20:19 -0500

    Add files via upload

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -7,4 +7,38 @@ proj04
 Asks the user for a string and prints out whether or not the string is a palindrome.
 
 """
+loop_control = True
+while loop_control == True:
+    str = raw_input ("Enter words here and you will know if it is a palindrome or not: ")
+    lst = []
+    for letter in str:
+        lst.append(letter)
+    lst.reverse()
+    lst2 = []
+    for letter in str:
+        lst2.append(letter)
+    if lst2 == lst:
+        print str, "is a palindrome."
+    elif lst2 != lst:
+        print str, "is not a palindrome."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
